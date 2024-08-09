9ab96b819aab44c5e0db9ecded253c1f4e2e67c9
mabrouk <mabrouk@mahdhi.com>
2024-08-10 00:48:08 +0200

    INFRA: Init Project

---
+++ FlexiMail/FlexiMailClient.cs
@@ -0,0 +1,11 @@
+﻿//---------------------------------------
+// Copyright (c) 2024 Mabrouk Mahdhi.
+// Made with love for the .NET Community
+//---------------------------------------
+
+namespace FlexiMail
+{
+    public class FlexiMailClient
+    {
+    }
+}
