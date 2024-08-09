d4b09200e2635583742e31e7aca42f1c2a507dce
mabrouk <mabrouk@mahdhi.com>
2024-08-10 00:51:08 +0200

    INFRA: Add Unit Tests

---
+++ FlexiMail.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+//---------------------------------------
+// Copyright (c) 2024 Mabrouk Mahdhi.
+// Made with love for the .NET Community
+//---------------------------------------
+
+namespace FlexiMail.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
