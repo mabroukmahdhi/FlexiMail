4e2b8f7ef45462b29528d8f30b5c8fd2c2d39162
mabrouk <mabrouk@mahdhi.com>
2024-08-11 16:03:31 +0200

    CLIENTS: SendAndSaveCopy

--- FlexiMail/FlexiMailClient.cs
+++ FlexiMail/FlexiMailClient.cs
@@ -3,9 +3,40 @@
 // Made with love for the .NET Community
 //---------------------------------------
 
+using System;
+using FlexiMail.Brokers.Exchanges;
+using FlexiMail.Models.Configurations;
+using FlexiMail.Models.Foundations.Messages;
+using FlexiMail.Services;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace FlexiMail
 {
-    public class FlexiMailClient
+    public class FlexiMailClient : IFlexiMailClient
     {
+        private readonly IFlexiExchangeService exchangeService;
+
+        public FlexiMailClient(ExchangeConfigurations configurations)
+        {
+            var serviceProvider = RegisterServices(configurations);
+
+            this.exchangeService =
+                serviceProvider.GetRequiredService<IFlexiExchangeService>();
+        }
+
+        public void SendAndSaveCopyAsync(FlexiMessage flexiMessage) =>
+            this.exchangeService.SendAndSaveCopyAsync(flexiMessage);
+
+        private static IServiceProvider RegisterServices(ExchangeConfigurations configurations)
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddTransient<IExchangeBroker, ExchangeBroker>()
+                .AddTransient<IFlexiExchangeService, FlexiExchangeService>()
+                .AddSingleton(configurations);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            return serviceProvider;
+        }
     }
 }
---
+++ FlexiMail/IFlexiMailClient.cs
@@ -0,0 +1,13 @@
+// ---------------------------------------
+// Copyright (c) 2024 Mabrouk Mahdhi.
+// Made with love for the .NET Community
+// ---------------------------------------
+
+using FlexiMail.Models.Foundations.Messages;
+
+namespace FlexiMail;
+
+public interface IFlexiMailClient
+{
+    void SendAndSaveCopyAsync(FlexiMessage flexiMessage);
+}
