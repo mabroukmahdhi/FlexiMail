8e678497e69fb66518589868048d3a0a5589c11c
mabrouk <mabrouk@mahdhi.com>
2024-08-11 17:34:22 +0200

    INFRA: Build Project

---
+++ FlexiMail.Infrastructure.Build/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV3s;
+
+namespace FlexiMail.Infrastructure.Build
+{
+    internal class Program
+    {
+        private const string BuildScriptPath = "../../../../.github/workflows/dotnet.yml";
+        
+        private static void Main(string[] args)
+        {
+            var adoNetClient = new ADotNetClient();
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "FlexiMail Build",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "main" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "main" }
+                    }
+                },
+
+                Jobs = new Dictionary<string, Job>
+                {
+                    {
+                        "build",
+                        new Job
+                        {
+                            EnvironmentVariables = new Dictionary<string, string>
+                            {
+                                { "Authority", "${{ secrets.AUTHORITY }}" },
+                                { "ClientId", "${{ secrets.CLIENTID }}" },
+                                { "ClientSecret", "${{ secrets.CLIENTSECRET }}" },
+                                { "PrincipalName", "${{ secrets.PRINCIPALNAME }}" },
+                                { "Sid", "${{ secrets.SID }}" },
+                                { "SmtpAddress", "${{ secrets.SMTPADDRESS }}" },
+                                { "TenantId", "${{ secrets.TENANTID }}" }
+                            },
+
+                            RunsOn = BuildMachines.WindowsLatest,
+
+                            Steps =
+                            [
+                                new CheckoutTaskV3
+                                {
+                                    Name = "Pulling Code"
+                                },
+
+
+                                new SetupDotNetTaskV3
+                                {
+                                    Name = "Installing .NET",
+
+                                    With = new TargetDotNetVersionV3
+                                    {
+                                        DotNetVersion = "8.0.*"
+                                    }
+                                },
+
+
+                                new RestoreTask
+                                {
+                                    Name = "Restoring Packages"
+                                },
+
+
+                                new DotNetBuildTask
+                                {
+                                    Name = "Building Solution"
+                                },
+
+
+                                new TestTask
+                                {
+                                    Name = "Running Tests"
+                                }
+                            ]
+                        }
+                    }
+                }
+            };
+
+            var directoryPath = Path.GetDirectoryName(BuildScriptPath);
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath!);
+            }
+
+            adoNetClient.SerializeAndWriteToFile(
+                adoPipeline: githubPipeline,
+                path: BuildScriptPath);
+        }
+    }
+}
--- FlexiMail/Services/FlexiExchangeService.cs
+++ FlexiMail/Services/FlexiExchangeService.cs
@@ -15,12 +15,13 @@ using Microsoft.Exchange.WebServices.Data;
 
 namespace FlexiMail.Services
 {
-    internal class FlexiExchangeService(ExchangeConfigurations configurations, IExchangeBroker exchangeBroker)
+    internal class FlexiExchangeService(
+        ExchangeConfigurations configurations, 
+        IExchangeBroker exchangeBroker)
         : IFlexiExchangeService
     {
         private readonly IExchangeBroker exchangeBroker = exchangeBroker;
-
-
+        
         public async void SendAndSaveCopyAsync(FlexiMessage flexiMessage)
         {
             var exchangeService = await CreateExchangeServiceAsync();
@@ -58,20 +59,37 @@ namespace FlexiMail.Services
                 File.Delete(filePath);
             }
         }
-
         private async ValueTask<ExchangeService> CreateExchangeServiceAsync()
         {
             var accessToken = await this.exchangeBroker.GetAccessTokenAsync();
 
+            var idType = GetConnectingIdType();
             var impersonatedUserId = new ImpersonatedUserId(
-                idType: ConnectingIdType.SmtpAddress,
-                id: configurations.SmtpAddress);
+                idType: idType,
+                id: GetUserId(idType));
 
             return this.exchangeBroker.CreateExchangeService(ExchangeVersion.Exchange2013, accessToken,
                 impersonatedUserId);
         }
+        private string GetUserId(ConnectingIdType idType) => idType switch
+        {
+            ConnectingIdType.PrincipalName => configurations.PrincipalName,
+            ConnectingIdType.SmtpAddress => configurations.SmtpAddress,
+            ConnectingIdType.SID => configurations.Sid,
+            _ => configurations.SmtpAddress
+        };
+        private ConnectingIdType GetConnectingIdType()
+        {
+            if (!string.IsNullOrWhiteSpace(configurations.SmtpAddress))
+                return ConnectingIdType.SmtpAddress;
 
+            if (!string.IsNullOrWhiteSpace(configurations.Sid))
+                return ConnectingIdType.SID;
 
+            return !string.IsNullOrWhiteSpace(configurations.PrincipalName)
+                ? ConnectingIdType.PrincipalName
+                : ConnectingIdType.SmtpAddress;
+        }
         private static BodyType MapBodyType(BodyContentType bodyContentType) => bodyContentType switch
         {
             BodyContentType.Html => BodyType.HTML,
